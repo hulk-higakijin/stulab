fb1a6280841e02fdee44548f52d2785e42541162
higakijin <hm385.chejptks@gmail.com>
2022-04-30 19:22:20 +0900

    seedファイルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Book.create!(
+  title: '戊辰戦争の舞台を歩いてみよう！',
+  introduction: '明治新政府と旧幕府軍の戦いである戊辰戦争。舞台は大坂の「鳥羽伏見の戦い」から「江戸城無血開城」、そして蝦夷の「五稜郭の戦い」まで。全国を巻き込んだまさに「日本最大の内戦」。その舞台をインターネットを使って歩いてみましょう。',
+  price: 1500,
+  likes_count: 0
+)
+
+book = Book.create!(
+  title: '坂本龍馬が誰に暗殺されたか考察してみよう！',
+  introduction: '幕末の偉人として取り上げられる坂本龍馬ですが、彼は誕生日に何者かによって暗殺されました。その犯人はいまだにわかっていません。 彼を暗殺を実行したのは一体誰なのか。その正体を当時の社会情勢を踏まえて考察していきましょう。',
+  price: 1000,
+  likes_count: 0
+)
+
+['新撰組説', '水戸藩説', '薩摩藩説', '土佐藩説'].each do |e|
+  book.chapters.create!(caption: e)
+end
