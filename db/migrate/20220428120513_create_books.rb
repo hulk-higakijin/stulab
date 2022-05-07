c76b9708f1a9cfa9e36284c6bd828dd87337d6c9
higakijin <hm385.chejptks@gmail.com>
2022-05-07 15:37:13 +0900

    bookモデルテスト

--- db/migrate/20220428120513_create_books.rb
+++ db/migrate/20220428120513_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
     create_table :books do |t|
       t.string :title, null: false
       t.text :introduction
-      t.integer :price, default: 0
+      t.integer :price, default: 1000
       t.integer :likes_count, default: 0
 
       t.timestamps
