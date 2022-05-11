f54f5915c4400b94688ffcbfa247191285f19fda
higakijin <hm385.chejptks@gmail.com>
2022-05-12 00:18:38 +0900

    レビュー投稿時、SPAに

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,9 @@
 class ReviewsController < ApplicationController
   def create
-    review = current_user.reviews.new(review_params)
-    review.book_id = params[:book_id].to_i
-    review.save!
+    @book = Book.find(params[:book_id])
+    @review = current_user.reviews.new(review_params)
+    @review.book_id = @book.id
+    @review.save
   end
 
   def destroy; end
