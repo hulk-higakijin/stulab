c2a33c7517eed058cde1f27018695eef82dc6cb1
higakijin <hm385.chejptks@gmail.com>
2022-05-12 18:15:28 +0900

    destroy実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,29 @@
 class ReviewsController < ApplicationController
+  before_action :set_book
   before_action :turbo_request?, only: [:index]
 
   def index
-    @book = Book.find(params[:book_id])
     render layout: false, content_type: 'text/vnd.turbo-stream.html'
   end
 
   def create
-    @book = Book.find(params[:book_id])
-    @review = current_user.reviews.new(review_params)
-    @review.book_id = @book.id
-    @review.save!
+    if !current_user.reviews.find_by(book_id: params[:book_id])
+      @review = current_user.reviews.new(review_params)
+      @review.book_id = @book.id
+      @review.save!
+    end
   end
 
-  def destroy; end
+  def destroy
+    @review = current_user.reviews.find_by(book_id: params[:book_id])
+    @review.destroy!
+  end
 
   private
+
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
 
     def turbo_request?
       raise ActionController::RoutingError, 'Not Found' unless turbo_frame_request?
