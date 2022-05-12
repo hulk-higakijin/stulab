00500a249d081acd0cb723edc3cb3910b8406140
hulk-higakijin <hm385.chejptks@gmail.com>
2022-05-12 21:49:40 +0900

    rails generate active_admin:resource [モデル]

---
+++ app/admin/chapters.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Chapter do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :number, :caption, :book_id, :deleted_at
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:number, :caption, :book_id, :deleted_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
